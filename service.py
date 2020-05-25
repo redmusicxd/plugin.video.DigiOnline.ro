5f216c2871693d80157311777276fec5e605b046
cralin <47091655+cralin@users.noreply.github.com>
2020-05-25 03:04:09 +0200

    Generate and maintain the XMLTV file that can be used by PVR IPTV Simple Client (#11)
    The addon now generates and maintains the XMLTV file that can be used by PVR IPTV Simple Client.

--- service.py
+++ service.py
@@ -25,6 +25,9 @@ import requests
 import json
 import logging
 import logging.handlers
+#import datetime
+from datetime import datetime
+from datetime import timedelta
 import time
 import resources.lib.common.vars as vars
 import resources.lib.common.functions as functions
@@ -215,16 +218,187 @@ def SimplePVRIntegration_update_m3u_file(NAME, COOKIEJAR, SESSION, DATA_DIR):
   logger.debug('Exit function')
 
 
+
+
+
+
+def SimplePVRIntegration_getEPG_data(NAME, COOKIEJAR, SESSION, DATE, STREAMID):
+  logger.debug('Enter function')
+
+  _url_ = "https://digiapis.rcs-rds.ro/digionline/api/v12/epg.php?action=getEPG&date=" + str(DATE) + "&id_stream=" + str(STREAMID)
+  logger.debug('URL = ' + str(_url_))
+
+  # Setup headers for the request
+  MyHeaders = {
+    'Host': 'digiapis.rcs-rds.ro',
+    'User-Agent': vars.__userAgent__,
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US',
+    'Accept-Encoding': 'identity',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Cache-Control': 'max-age=0'
+  }
+
+  logger.debug('Cookies: ' + str(list(COOKIEJAR)))
+  logger.debug('Headers: ' + str(MyHeaders))
+  logger.debug('URL: ' + str(_url_))
+  logger.debug('Method: GET')
+
+  # Send the GET request
+  _request_ = SESSION.get(_url_, headers=MyHeaders)
+
+  logger.debug('Received status code: ' + str(_request_.status_code))
+  logger.debug('Received cookies: ' + str(list(COOKIEJAR)))
+  logger.debug('Received headers: ' + str(_request_.headers))
+  logger.debug('Received data: ' + str(_request_.content))
+
+
+  logger.debug('Exit function')
+  return str(_request_.content)
+  
+
+def SimplePVRIntegration_init_EPG_file(NAME, COOKIEJAR, SESSION, DATA_DIR):
+  logger.debug('Enter function')
+  
+  _ENVHOME_DIR_ = xbmc.translatePath('special://envhome')
+  _epg_file_ = os.path.join(_ENVHOME_DIR_, vars.__SimplePVRIntegration_EPG_FileName__)
+  logger.debug('epg file: ' + _epg_file_)
+
+  if os.path.exists(_epg_file_) and os.path.getsize(_epg_file_) != 0:
+    # The _epg_file_ exists and is not empty.
+    logger.debug('\'' + _epg_file_ + '\' exists and is not empty.')
+  else:
+    # The _epg_file_ does not exist or is empty.
+    logger.debug('\'' + _epg_file_ + '\' does not exist or is empty.')
+    SimplePVRIntegration_update_EPG_file(NAME, COOKIEJAR, SESSION, DATA_DIR)
+    
+  logger.debug('Exit function')
+
+
+def SimplePVRIntegration_update_EPG_file(NAME, COOKIEJAR, SESSION, DATA_DIR):
+  logger.debug('Enter function')
+
+  _today_ = datetime.date(datetime.today())
+  _tomorrow_ = datetime.date(datetime.today()) + timedelta(days=1)
+  logger.debug('_today_: ' + str(_today_))
+  logger.debug('_tomorrow_: ' + str(_tomorrow_))
+  
+  _ENVHOME_DIR_ = xbmc.translatePath('special://envhome')
+  _epg_file_ = os.path.join(_ENVHOME_DIR_, vars.__SimplePVRIntegration_EPG_FileName__)
+  _tmp_epg_file_ = os.path.join(_ENVHOME_DIR_, vars.__SimplePVRIntegration_EPG_FileName__ + '.tmp')
+  logger.debug('epg file: ' + _epg_file_)
+  logger.debug('Temp epg file: ' + _tmp_epg_file_)
+  
+  _data_file_ = open(_tmp_epg_file_, 'w')
+  
+  _data_file_.write("<?xml version=\"1.0\" encoding=\"utf-8\" ?>" + "\n")
+  _data_file_.write("<tv>" + "\n")
+  
+  # Login to DigiOnline for this session
+  login = functions.do_login(NAME, COOKIEJAR, SESSION)
+
+  if login['exit_code'] != 0:
+    logger.debug('[Authentication error] => Error message: '+ login['error_message'])
+
+  else:
+    # Get video categories
+    categories = functions.get_categories(NAME, COOKIEJAR, SESSION)
+    #logger.debug('Received categories = ' + str(categories))
+    logger.debug('Received categories = ' + str(categories))
+    
+    for category in categories:
+      logger.debug('category name = ' + category['name'])
+      
+      # Get the list of channels in the category.
+      channels = functions.get_channels(category['name'], NAME, COOKIEJAR, SESSION)
+      logger.debug('Received channels = ' + str(channels))
+      
+      for channel in channels:
+        logger.debug('Channel data => ' +str(channel))
+       
+        _channel_metadata_ = json.loads(channel['metadata'])
+        logger.debug('Channel streamId: ' + str(_channel_metadata_['new-info']['meta']['streamId']))
+        
+        
+        _json_today_ = SimplePVRIntegration_getEPG_data(NAME, COOKIEJAR, SESSION, _today_, _channel_metadata_['new-info']['meta']['streamId'])
+        logger.debug('_json_today_: ' + str(_json_today_))
+        
+        ### Workaround for poorly coded/tested API providing EPG data
+        if _json_today_ == "ERR":
+          logger.debug('Creating json data structure for \'' + channel['name'] + '\'' )
+          _json_today_ = '{"meta":{"version":"6"},"data":{"id_stream":"' + str(_channel_metadata_['new-info']['meta']['streamId']) + '","stream_name":"","stream_desc":"' + channel['name'] + '","ios_button":"","ios_button_on":"","ios_button_size":"","ios_button_url":"","epg":[]}}'
+          logger.debug('_json_today_: ' + str(_json_today_))
+        
+        _json_tomorrow_ = SimplePVRIntegration_getEPG_data(NAME, COOKIEJAR, SESSION, _tomorrow_, _channel_metadata_['new-info']['meta']['streamId'])
+        logger.debug('_json_tomorrow_: ' + str(_json_tomorrow_))
+        
+        ### Workaround for poorly coded/tested API providing EPG data
+        if _json_tomorrow_ == "ERR":
+          logger.debug('Creating json data structure for \'' + channel['name'] + '\'' )
+          _json_tomorrow_ = '{"meta":{"version":"6"},"data":{"id_stream":"' + str(_channel_metadata_['new-info']['meta']['streamId']) + '","stream_name":"","stream_desc":"' + channel['name'] + '","ios_button":"","ios_button_on":"","ios_button_size":"","ios_button_url":"","epg":[]}}'
+          logger.debug('_json_tomorrow_: ' + str(_json_tomorrow_))
+
+        _epg_today_ = json.loads(_json_today_)
+        _epg_tomorrow_ = json.loads(_json_tomorrow_)
+        logger.debug('_epg_today_: ' + str(_epg_today_))
+        logger.debug('_epg_tomorrow_: ' + str(_epg_tomorrow_))
+
+        _epg_ = _epg_today_.copy()
+        _epg_['data']['epg'] = _epg_today_['data']['epg'] + _epg_tomorrow_['data']['epg']
+
+        _line_ = "  <channel id=\"" + _epg_['data']['id_stream'] + "\">"
+        _data_file_.write(_line_ + "\n")
+        _line_ = "    <display-name>" + _epg_['data']['stream_desc'] + "</display-name>"
+        _data_file_.write(_line_ + "\n")
+        _line_ = "  </channel>"
+        _data_file_.write(_line_ + "\n")
+        
+        if _epg_['data']['epg']:
+          _len_ = len(_epg_['data']['epg'])
+          
+          for index, _program_data_ in enumerate(_epg_['data']['epg'], start=1):
+            _start_date_time_object_ = datetime.utcfromtimestamp(_program_data_['start_ts'])
+            if index < _len_:
+              _stop_date_time_object_ = datetime.utcfromtimestamp(_epg_['data']['epg'][index]['start_ts'])
+            else:
+              _stop_date_time_object_ = datetime.utcfromtimestamp(_program_data_['start_ts'])
+
+            _line_ = "  <programme start=\"" + str(_start_date_time_object_.strftime("%Y%m%d%H%M%S")) + "\" stop=\"" + str(_stop_date_time_object_.strftime("%Y%m%d%H%M%S")) + "\" channel=\"" + _epg_['data']['id_stream'] + "\">"
+            _data_file_.write(_line_ + "\n")
+
+            _line_ = "    <title>" + _program_data_['program_name'] + "</title>"
+            _data_file_.write(_line_ + "\n")
+
+            _line_ = "    <desc>" + _program_data_['program_description'] + "\n\n" + _program_data_['program_description_l'] + "</desc>"
+            _data_file_.write(_line_ + "\n")
+
+            _line_ = "  </programme>"
+            _data_file_.write(_line_ + "\n")
+
+
+  
+  _data_file_.write("</tv>" + "\n")
+  _data_file_.close()
+  os.rename(_tmp_epg_file_, _epg_file_)
+  
+  logger.debug('Exit function')
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
   logger.debug('Enter __main__ ')
 
   SimplePVRIntegration_init_m3u_file(vars.__ServiceID__, vars.__AddonCookieJar__, vars.__ServiceSession__, MyServiceAddon_DataDir)
+  SimplePVRIntegration_init_EPG_file(vars.__ServiceID__, vars.__AddonCookieJar__, vars.__ServiceSession__, MyServiceAddon_DataDir)
   
   schedule_jobs()
-  #schedule.every(10).minutes.do(schedule_jobs)
   schedule.every().minute.at(":05").do(schedule_jobs)
-
-  #schedule.every().minute.at(":05").do(SimplePVRIntegration_init_m3u_file, NAME=vars.__ServiceID__, COOKIEJAR=vars.__AddonCookieJar__, SESSION=vars.__ServiceSession__, DATA_DIR=MyServiceAddon_DataDir)
 
   monitor = xbmc.Monitor()  
   while not monitor.abortRequested():
